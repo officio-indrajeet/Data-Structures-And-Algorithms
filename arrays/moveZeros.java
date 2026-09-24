5cc41b99910da562d26ceae6bdb9b4edc3f6a2fe
officio-indrajeet <officio-indrajeet@gmail.com>
2026-09-24 23:01:29 +0100

    Solved move-zeros

---
+++ arrays/moveZeros.java
@@ -0,0 +1,18 @@
+// leetcode 283. Move Zeroes
+// TC: O(n) SC: O(1)
+package arrays;
+
+public class moveZeros {
+    public void moveZeros(int[] nums) {
+        int n = nums.length;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+        }
+    }
+}
