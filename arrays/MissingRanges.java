559c719fe549b2e9171b8aafc9f61917ac3e6799
officio-indrajeet <officio-indrajeet@gmail.com>
2026-09-26 21:59:55 +0100

    solve: 2026-09-26 21:59

---
+++ arrays/MissingRanges.java
@@ -0,0 +1,46 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MissingRanges {
+    public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        if (n == 0) {
+            result.add(Arrays.asList(lower, upper));
+            return result;
+        }
+
+        // Gap before the first element
+        if (lower < nums[0]) {
+            result.add(Arrays.asList(lower, nums[0] - 1));
+        }
+
+        // Gaps between elements
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i + 1] - nums[i] > 1) {
+                result.add(Arrays.asList(nums[i] + 1, nums[i + 1] - 1));
+            }
+        }
+
+        // Gap after the last element
+        if (upper > nums[n - 1]) {
+            result.add(Arrays.asList(nums[n - 1] + 1, upper));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MissingRanges solution = new MissingRanges();
+        int[] nums = { 0, 1, 3, 50, 75 };
+        int lower = 0;
+        int upper = 99;
+        List<List<Integer>> missingRanges = solution.findMissingRanges(nums, lower, upper);
+        System.out.println(missingRanges); // Output: [[2], [4, 49], [51, 74], [76, 99]]
+    }
+
+}
